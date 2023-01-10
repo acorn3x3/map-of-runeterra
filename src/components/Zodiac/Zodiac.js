f178e569f69139bcd694153391abd5421ea86421
Andrew Cornejo <atcornejo3@gmail.com>
2023-01-10 15:21:08 -0800

    fixing some css positions. Symbols still dont work yet

--- src/components/Zodiac/Zodiac.js
+++ src/components/Zodiac/Zodiac.js
@@ -2,8 +2,8 @@ import './Zodiac.css';
 export default function Zodiacs(props) {
   return (
     <div className="Zodiac" style={{ top: props.top, left: props.left }}>
-      {/* <img src= symbol></img> */}
-      <h2 className="name"></h2>
+      {/* <img src={`/zodiacs/${props.name}.jpg`}> </img> */}
+      <h2 className="name">{props.name}</h2>
       <p>{props.dates}</p>
     </div>
   );
